2d5d17d345ca1f8514f0e6d95d9b663df65a2f3f
Alexandre Detiste <alexandre.detiste@gmail.com>
2023-07-30 18:56:55 +0200

    Debianize

--- systemd-crontab-generator.c
+++ systemd-crontab-generator.c
@@ -2,7 +2,7 @@
   This file is part of systemd-cron.
 
   Copyright 2013 Shawn Landden
-            2016-2023 Alexandre Detiste
+            2014-2023 Alexandre Detiste
 
   systemd-cron is free software; you can redistribute it and/or modify it
   under the terms of the GNU Lesser General Public License as published by
